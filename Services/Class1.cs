be274d20f45a8ecf728843120ca0be6caeb3a58a
Artsiom Hramyka <artsiom.hramyka@innowise-group.com>
2022-05-16 16:48:43 +0300

    add auto mapper

--- Services/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿
-namespace Services
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ Services/Common/Mappings/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Domain.Entities;
+
+namespace Services.Common.Mappings;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMapsForRooms();
+    }
+
+    private void CreateMapsForRooms()
+    {
+        CreateMap<Room, RoomDTO>().ReverseMap();
+    }
+}
