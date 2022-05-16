9bc848d170517ebaf5947125bac8f63d461620bd
Artsiom Hramyka <artsiom.hramyka@innowise-group.com>
2022-05-16 10:10:43 +0300

    Add project files.

---
+++ Domain/Entities/Room.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Entities;
+
+public class Room
+{
+    public Guid Id { get; set; }
+    public string Address { get; set; }
+    public bool HasInternet { get; set; }
+    public decimal Price { get; set; }
+}
