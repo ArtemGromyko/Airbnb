bc53a700bfcf936e034d8912f8cdede073b0cf00
Artsiom Hramyka <artsiom.hramyka@innowise-group.com>
2022-05-19 18:07:58 +0300

    add serilog

---
+++ Services/Common/Behaviours/LoggingBehavior.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Serilog;
+
+namespace Application.Common.Behaviours;
+
+public class LoggingBehavior<TRequest, TResponse>
+    : IPipelineBehavior<TRequest, TResponse> where TRequest
+    : IRequest<TResponse>
+{
+    public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+    {
+        var requestName = typeof(TRequest).Name;
+
+        Log.Information("Request: {Name}", requestName);
+
+        var response = await next();
+
+        return response;
+    }
+}
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -2,13 +2,16 @@ using WebApi.Extensions;
 using MediatR;
 using Application.Commands.CreateRoom;
 using Application.Common.Behaviours;
-using FluentValidation;
 using WebApi.Middlewares;
 using MicroElements.Swashbuckle.FluentValidation.AspNetCore;
 using FluentValidation.AspNetCore;
 using MicroElements.Swashbuckle.FluentValidation;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((ctx, lc) => 
+    lc.WriteTo.Console());
 
 var services = builder.Services;
 var configuration = builder.Configuration;
@@ -28,6 +31,7 @@ services.AddSwaggerGen();
 services.ConfigureSqlContext(configuration);
 services.ConfigureRepositories();
 services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+services.AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
 
 var app = builder.Build();
 
